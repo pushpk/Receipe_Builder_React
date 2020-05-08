aeed02e4556da0a7ce1550150dc6ad84678df9f7
Pushpak Panchal <pushpak.net@gmail.com>
2020-05-08 12:46:05 -0500

    Person compo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
+class App extends Component {
+
+  state = {
+    persons : [
+      { name : 'Max', age : 28},
+      { name : 'Manu', age : 25},
+      { name : 'Step', age : 26},
+    ]
+  }
+
+   ClickHandler = () =>{
+
+    this.setState({persons : [
+      { name : 'Max 1', age : 28},
+      { name : 'Manu 2', age : 25},
+      { name : 'Step 2', age : 26},
+    ]});
+   }
+
+  render (){
+    return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>React App - Hello</h1>
+
+       <button onClick={this.ClickHandler}>Click me!</button>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >ddd</Person>
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} > Hobbies :  Play</Person>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
