598f4a2ca915cc4c8b460e1c0646ff469d3156c9
Pushpak Panchal <pushpak.net@gmail.com>
2020-05-12 07:30:49 -0500

    organizing app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from '../components/Person/Person';
+import Persons from '../components/Persons/Persons';
 import Radium from 'radium';
 
 
 class App extends Component {
   state = {
     persons: [
-      {id:1, name: 'Max', age: 28 },
-      {id:2, name: 'Manu', age: 25 },
-      {id:3, name: 'Step', age: 26 },
+      { id: 1, name: 'Max', age: 28 },
+      { id: 2, name: 'Manu', age: 25 },
+      { id: 3, name: 'Step', age: 26 },
 
 
     ],
@@ -23,35 +23,17 @@ class App extends Component {
 
   deleteClickHandler = (index) => {
     const persons = this.state.persons.slice();
-     persons.splice(index, 1);
-    this.setState({persons : persons })
-
+    persons.splice(index, 1);
+    this.setState({ persons: persons })
   }
 
-
- 
   render() {
-
     const styles = {
-
-      backgroundColor : 'green',
+      backgroundColor: 'green',
       textAlign: 'center',
       padding: '5px',
-      ':hover':{ backgroundColor : 'lightgray'}
-
+      ':hover': { backgroundColor: 'lightgray' }
     }
-    var person = null;
-      if(this.state.toggleShowHidePerson){
-        person = (
-          <div>
-            { this.state.persons.map((person, index) => {
-              return <Person name={person.name} age= {person.age} key={person.id} click={ () => this.deleteClickHandler(index)} />
-            })}
-         </div>
-        )
-
-        styles.backgroundColor = 'blue';
-      }
 
     return (
       <div className="App">
@@ -59,10 +41,12 @@ class App extends Component {
           <h1>React App - Hello</h1>
         </header>
         <button style={styles} onClick={this.ClickHandler}> Swtich Person</button>
-        {person}
+        {
+        this.state.toggleShowHidePerson ?  <Persons persons={this.state.persons} deleteClickHandler={this.deleteClickHandler} /> : null
+        }
       </div>
     )
-    
+
   }
 }
 
